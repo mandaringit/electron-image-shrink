8ee60cd13114cb1df1581839eac509b8e5112408
mandaringit <mandarin.dev@kakao.com>
2022-04-24 19:20:29 +0900

    일렉트론 기본

---
+++ main.js
@@ -0,0 +1,32 @@
+const { app, BrowserWindow } = require("electron")
+
+process.env.NODE_ENV = "development"
+
+const isDev = process.env.NODE_ENV !== "production"
+const isMac = process.platform === "darwin"
+
+function createMainWindow() {
+  const mainWindow = new BrowserWindow({
+    title: "Image Shrink",
+    width: 800,
+    height: 600,
+    icon: `${__dirname}/assets/icons/Icon_256x256.png`,
+    resizable: isDev
+  })
+
+  mainWindow.loadFile("./app/index.html")
+}
+
+app.on("ready", createMainWindow)
+
+app.on('window-all-closed', () => {
+  if(!isMac) {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if(BrowserWindow.getAllWindows().length === 0) {
+    createMainWindow()
+  }
+})
