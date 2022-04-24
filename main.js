e2ea42ba2aa867cf8b07c84dd834bee483fe472d
mandaringit <mandarin.dev@kakao.com>
2022-04-24 22:06:22 +0900

    기본적인 메뉴 / 숏컷

--- main.js
+++ main.js
@@ -1,59 +1,90 @@
 const { app, BrowserWindow, Menu, globalShortcut } = require("electron")
-const path = require('path')
+const path = require("path")
 process.env.NODE_ENV = "development"
 
 const isDev = process.env.NODE_ENV !== "production"
 const isMac = process.platform === "darwin"
 
-let mainWindow;
+let mainWindow
 
 function createMainWindow() {
-  mainWindow = new BrowserWindow({
-    title: "Image Shrink",
-    width: 600,
-    height: 600,
-    icon: path.join(__dirname, `assets/icons/Icon_256x256.png`),
-    resizable: isDev,
-    backgroundColor: "white"
-  })
+	mainWindow = new BrowserWindow({
+		title: "Image Shrink",
+		width: 600,
+		height: 600,
+		icon: path.join(__dirname, `assets/icons/Icon_256x256.png`),
+		resizable: isDev,
+		backgroundColor: "white",
+	})
 
-  mainWindow.loadFile("./app/index.html")
+	mainWindow.loadFile("./app/index.html")
 }
 
 const menu = [
-    ...(isMac ? [{ role: 'appMenu'}] : []),
-  {
-    label: 'File',
-    submenu: [{
-      label: 'Quit',
-      accelerator: 'CmdOrCtrl+W',
-      click: () => app.quit()
-    }]
-  },
+	...(isMac
+		? [
+				{
+					role: "appMenu",
+				},
+		  ]
+		: []),
+	{
+		role: "fileMenu",
+		submenu: [
+			{
+				label: "Exit",
+				accelerator: "CmdOrCtrl+W",
+				click: () => app.quit(),
+			},
+		],
+	},
+	...(isDev
+		? [
+				{
+					label: "Developer",
+					submenu: [
+						{
+							role: "reload",
+						},
+						{
+							role: "forceReload",
+						},
+						{
+							type: "separator",
+						},
+						{
+							role: "toggleDevTools",
+						},
+					],
+				},
+		  ]
+		: []),
 ]
 
+console.log(menu)
+
 app.on("ready", () => {
-  createMainWindow()
+	createMainWindow()
 
-  // 메뉴 달아보기
-  const mainMenu = Menu.buildFromTemplate(menu)
-  Menu.setApplicationMenu(mainMenu)
+	// 메뉴 달아보기
+	const mainMenu = Menu.buildFromTemplate(menu)
+	Menu.setApplicationMenu(mainMenu)
 
-  // 전역 숏컷
-  globalShortcut.register('CmdOrCtrl+R', () => mainWindow.reload())
-  globalShortcut.register(isMac ? 'Command+Alt+I' : 'Ctrl+Shift+I', () => mainWindow.toggleDevTools())
+	// 전역 숏컷
+	globalShortcut.register("CmdOrCtrl+R", () => mainWindow.reload())
+	globalShortcut.register(isMac ? "Command+Alt+I" : "Ctrl+Shift+I", () => mainWindow.toggleDevTools())
 
-  mainWindow.on('ready', () => mainWindow = null)
+	mainWindow.on("ready", () => (mainWindow = null))
 })
 
-app.on('window-all-closed', () => {
-  if(!isMac) {
-    app.quit()
-  }
+app.on("window-all-closed", () => {
+	if (!isMac) {
+		app.quit()
+	}
 })
 
-app.on('activate', () => {
-  if(BrowserWindow.getAllWindows().length === 0) {
-    createMainWindow()
-  }
+app.on("activate", () => {
+	if (BrowserWindow.getAllWindows().length === 0) {
+		createMainWindow()
+	}
 })
