9caaa3551ec1dc4f2a699a417a226a290519b8be
mandaringit <mandarin.dev@kakao.com>
2022-04-24 21:44:57 +0900

    기본적인 메뉴 / 숏컷

--- main.js
+++ main.js
@@ -1,23 +1,50 @@
-const { app, BrowserWindow } = require("electron")
-
+const { app, BrowserWindow, Menu, globalShortcut } = require("electron")
+const path = require('path')
 process.env.NODE_ENV = "development"
 
 const isDev = process.env.NODE_ENV !== "production"
 const isMac = process.platform === "darwin"
 
+let mainWindow;
+
 function createMainWindow() {
-  const mainWindow = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     title: "Image Shrink",
     width: 600,
     height: 600,
-    icon: `${__dirname}/assets/icons/Icon_256x256.png`,
-    resizable: isDev
+    icon: path.join(__dirname, `assets/icons/Icon_256x256.png`),
+    resizable: isDev,
+    backgroundColor: "white"
   })
 
   mainWindow.loadFile("./app/index.html")
 }
 
-app.on("ready", createMainWindow)
+const menu = [
+    ...(isMac ? [{ role: 'appMenu'}] : []),
+  {
+    label: 'File',
+    submenu: [{
+      label: 'Quit',
+      accelerator: 'CmdOrCtrl+W',
+      click: () => app.quit()
+    }]
+  },
+]
+
+app.on("ready", () => {
+  createMainWindow()
+
+  // 메뉴 달아보기
+  const mainMenu = Menu.buildFromTemplate(menu)
+  Menu.setApplicationMenu(mainMenu)
+
+  // 전역 숏컷
+  globalShortcut.register('CmdOrCtrl+R', () => mainWindow.reload())
+  globalShortcut.register(isMac ? 'Command+Alt+I' : 'Ctrl+Shift+I', () => mainWindow.toggleDevTools())
+
+  mainWindow.on('ready', () => mainWindow = null)
+})
 
 app.on('window-all-closed', () => {
   if(!isMac) {
